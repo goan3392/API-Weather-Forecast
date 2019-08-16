da0b4346c73a9beeba1443d158f81f3e1b2ebb65
goan3392 <47539861+goan3392@users.noreply.github.com>
2019-08-15 20:49:52 -0400

    Update Titles.js

--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -3,8 +3,8 @@ import React from 'react';
 // Use this form if there no state
 const Titles = () => (
     <div>
-        <h1>Weather App</h1>
-        <p>Enter Your City and Country</p>
+        <h1 className="title-container__title">Weather App</h1>
+        <h5 className='.title-container__subtitle'>Enter Your City and Country</h5>
     </div>
 );
 
